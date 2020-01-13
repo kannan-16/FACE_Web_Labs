1a33e6109eef6563ca05ff3494c641617772be08
Hamza <hamza.kaizar@gmail.com>
2020-01-13 10:02:30 +0530

    Lab 2 Backend

---
+++ Day 2/Lab 2/data.js
@@ -0,0 +1,77 @@
+var players = [
+    {
+        name: "David de Gea",
+        age: 29,
+        debut: 2011,
+        team: "Manchester Utd",
+        position: "Goalkeeper",
+        country: "England",
+        awards: [
+            {
+                name: "Golden Foot",
+                year: 2015
+            }
+        ]
+    },
+
+    
+    {
+        name: "Hamza de Kaizar",
+        age: 22,
+        debut: 2011,
+        team: "Liverpool",
+        position: "Striker",
+        country: "India",
+        awards: [
+            {
+                name: "Golden Foot",
+                year: 2015
+            }
+        ]
+    },
+
+    {
+        name: 'Ashley Young',
+        age: 34,
+        debut: 2011,
+        team: 'Manchester Utd',
+        position: 'Defender',
+        country: 'England',
+        awards: [
+            {
+                name: "Golden Foot",
+                year: 2015
+            }
+        ]
+      },
+
+      {
+        name: 'Ashley Young',
+        age: 34,
+        debut: 2011,
+        team: 'Manchester Utd',
+        position: 'Defender',
+        country: 'England',
+        awards: [
+            {
+                name: "Golden Foot",
+                year: 2015
+            }
+        ]
+      },
+
+      {
+        name: 'Ashley Young',
+        age: 34,
+        debut: 2011,
+        team: 'Manchester Utd',
+        position: 'Defender',
+        country: 'England',
+        awards: [
+            {
+                name: "Golden Foot",
+                year: 2015
+            }
+        ]
+      }
+];
