9e99c7ca8f4d216ac5902a8f4031f135d9c1d031
Hamza <hamza.kaizar@gmail.com>
2020-01-14 12:23:57 +0530

    Day 3 Lab 1

---
+++ Day 3/Lab 1/README.md
@@ -0,0 +1,80 @@
+![logo_face](https://www.focusacademy.in/images/face_logo.svg)
+
+## LAB | Chocolate Dispenser
+    
+Caitlyn loved chocolates and asked her mum to buy her a truck of chocolates. Her mum wasn't as generous, but she did buy Cait a tiny chocolate dispenser. However, she didn't teach Cait how to operate it. She would only give her one chocolate a day. Cait obviously wanted more, so much more.
+
+Cait is now on a quest to create a virtual terminal which can directly access the chocolate machine. She needs **YOU** to help her code functionalities in the back-end though. Go to the `src/app.js` file and complete all the unfinished code to satiate her sweet tooth.
+
+## Requirements
+
+- Fork this repo
+- Clone this repo
+- Practice JavaScript basics - _operators, conditions, loops_
+
+## Submission
+
+- Upon completion, run the following commands:
+
+  ```
+  git add .
+  git commit -m "done"
+  git push origin master
+  ```
+
+- Create Pull Request so your TAs can check up your work.
+
+## Starter code
+
+The `src/app.js` contains an array of 100 chocolates. We are talking about the 100 _strings_ that specifies the color of each chocolate. Available chocolates are of the colors `green, red, purple, blue, crimson, silver, pink`.
+
+### Tests
+
+Ohh yes! We have our beloved tests, and you already know how this works. Open the `SpecRunner.html` file on your browser and start coding to pass the test. Remember to focus on one test at a time and read carefully the instructions to understand what you have to do.
+
+## Trial 1: MORE! MORE! MORE!
+
+Hundred chocolates? Not enough! We certainly need more. Create a function `addChocolates()` which takes `color, count` as arguments and adds them to the dispenser from the top.
+
+## Trial 2: LESS! LESS! LESS!
+
+We have added too many chocolates and the machine has started to malfunction. Create a function `removeChocolates()` that takes `number` as an argument and _returns an array of chocolates_ that have been removed from the top.
+
+## Trial 3: One is not enough!
+
+Cait can't have just one chocolate a day. So our next task would be to create a function `dispenseChocolates()` that dispenses chocolate at will. It takes `number` as an argument and _returns an array of chocolates_ from the bottom.
+
+## Trial 4: My favourite color is...
+
+Cait is annoyed with all the green chocolates she's been getting. Time to change things up a bit. Create a function `dispenseChocolatesOfColor()` which takes `number, color` as arugments and _return an array of chocolates_ from the bottom of the given color.
+
+## Trial 5: LED my chocos!
+
+Cait is finding it difficult to keep track of her chocolates. You'll need to create an LED that shows her how many of each chocolate are remaining. Create a function `noOfChocolates()` that _returns an array of number of chocolates_ in the order `[green, red, purple, blue, crimson, silver, pink]`.
+
+## Trial 6: One a penny, two a penny, hot cross shoots!
+
+
+
+## Trial 7: But, where do you come from?
+
+
+
+## Trial 8: This, that and then some more...
+
+
+
+## Challenge 1: Old is gold.
+
+
+
+## Challenge 2: Team Game
+
+
+
+## Judgement 1: The devil is in the details
+
+
+
+## Judgement 2: The golden goal
+
--- Day 3/Lab 1/src/app.js
+++ Day 3/Lab 1/src/app.js
@@ -1,3 +1,16 @@
+var chocolates = [  
+    "green","green","green","silver","blue","crimson","purple","red","crimson","purple",
+    "purple","green","pink","blue","red","silver","crimson","purple","silver","silver",
+    "red","green","red","silver","pink","crimson","purple","green","red","silver",
+    "crimson","pink","silver","blue","pink","crimson","crimson","crimson","red","purple",
+    "purple","green","pink","blue","red","crimson","silver","purple","purple","purple",
+    "red","purple","red","blue","silver","green","crimson","silver","blue","crimson",
+    "purple","green","pink","green","red","silver","crimson","blue","green","red",
+    "red","pink","blue","silver","pink","crimson","purple","green","red","blue",
+    "red","purple","silver","blue","pink","silver","crimson","silver","blue","purple",
+    "purple","green","blue","blue","red","red","silver","purple","silver","crimson"
+];
+
 //Trial 1: Add ___ chocolates of ____ color
 function addChocolates(color, number) {
     for(let i=0; i<number; i++)
@@ -22,11 +35,10 @@ function dispenseChocolates(number) {
 
 //Trial 4: Dispense ___ chocolates of ____ color
 function dispenseChocolatesOfColor(number, color) {
-    let dispensedChoco = [];
-    let filteredArray = chocolates.filter(ch => ch===color);
-    for(let i=0; i<number; i++)
-        dispensedChoco.push(filteredArray.pop());
-    return dispensedChoco;
+    for(let i=0; i<number; i++) {
+        let indexOf = chocolates.lastIndexOf(color);
+        chocolates.splice(indexOf,1);
+    }
 }
 
 //Trial 5: Display ___ chocolates of each color. Return array of numbers [green, red, purple, blue, crimson, silver, pink]
@@ -34,46 +46,94 @@ function noOfChocolates() {
     let types = chocolates.filter(e => e.indexOf()==-1);
     let numbers = [];
     for(let i=0; i<types.length; i++) {
-        numbers[i] = chocolates.reduce((acc, val) => acc += (val==types[i])?1:0);
+        numbers[i] = chocolates.reduce((acc, val) => acc += (val==types[i])?1:0, 0);
     }
     return numbers;
 }
 
 //Trial 6: Sort chocolates based on total in each color. Return array of colors
 function sortChocolateBasedOnTotal() {
-    let numbers = noOfChocolates();
-    
+    numbers = noOfChocolates();
+    var chocolateCount = {
+        "green": numbers[0],
+        "red": numbers[1],
+        "purple": numbers[2],
+        "blue": numbers[3],
+        "crimson": numbers[4],
+        "silver": numbers[5],
+        "pink": numbers[6]
+    }
+    chocolates = chocolates.sort((a,b) => {
+        if(chocolateCount[a] < chocolateCount[b]) {
+            temp = a;
+            a = b;
+            b = temp;
+        }
+    });
+    console.log(chocolates);
 }
+sortChocolateBasedOnTotal();
 
 //Trial 7: Change ___ chocolates of ____ color to ____ color
 function changeChocolateColor(noOfChocolates, color, finalColor) {
+    let chocChanged = 0;
+    chocolates.map(e => {
+        if(ch==color) {
+            chocChanged++;
+            if(chocChanged<=noOfChocolates)
+                return finalColor;
+            else
+                break;
+        }
+        else {
+            return e;
+        }
+    });
 }
 
-//Trial 8: Change all chocolates of ____ color to ____ color and display total
+//Trial 8: Change all chocolates of ____ color to ____ color and display total of the changed color
 function changeChocolateColorAllOfxTotal(color, finalColor) {
+    return chocolates.map(e => (e==color)?finalColor:color).reduce((acc,val) => acc+=(val==finalColor)?1:0);
 }
 
-//Quest 1: Remove chocolates of ____ color from the top
+//Challenge 1: Remove chocolates of ____ color from the top
 function removeChocolatesOfColor(color) {
+    for(let i = 0; i < chocolates.length; i++){ 
+        if (chocolates[i] === color) {
+          arr.splice(i, 1);
+          break;
+        }
+    }
 }
 
-//Quest 2: Dispense 1 rainbow colored colored chocolate for every 3 differently chocolates dispensed
+//Challenge 2: Dispense 1 rainbow colored colored chocolate for every 3 chocolates of the same color dispensed
 function dispenseRainbowChocolates(noOfChocolates) {
+    dispenseChocolates = dispenseChocolates(noOfChocolates);
+    let frequency = [dispenseChocolates.length];  
+    let visited = -1;  
+    
+    for(let i = 0; i < dispenseChocolates.length; i++){  
+        let count = 1;  
+        for(let j = i+1; j < dispenseChocolates.length; j++){  
+            if(dispenseChocolates[i] == dispenseChocolates[j]){  
+                count++;
+                frequency[j] = visited; 
+            }  
+        }  
+        if(frequency[i] != visited)  
+            frequency[i] = count;
+    }
+
+    let rainbowChocolates = 0;
+    for(let i = 0; i < frequency.length; i++){  
+        if(frequency[i] != visited && frequency[i]%3 == 0)  {
+            rainbowChocolates += frequency[i]/3;
+        }  
+    }
+    return rainbowChocolates;
 }
 
-//Challenge 1:
+//Judgement 1: 
 
 
-//Challenge 2:
-
-
-var chocolates = ["green","green","green","silver","blue","crimson","purple",
-                  "purple","green","pink","blue","red","silver","crimson",
-                  "red","green","red","silver","pink","crimson","purple",
-                  "crimson","pink","silver","blue","pink","crimson","crimson",
-                  "purple","green","pink","blue","red","crimson","silver",
-                  "red","purple","red","blue","silver","green","crimson",
-                  "purple","green","pink","green","red","silver","crimson",
-                  "red","pink","blue","silver","pink","crimson","purple",
-                  "red","purple","silver","blue","pink","silver","crimson",
-                  "purple","green","blue","blue","red","red","silver"];
+//Judgement 2:
