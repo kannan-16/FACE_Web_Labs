a8e14754a05589f3c7fb2af497a45878cf3a275c
Hamza <hamza.kaizar@gmail.com>
2020-01-14 17:09:02 +0530

    Completed Lab - Sort pending

--- Day 3/Lab 1/src/link.js
+++ Day 3/Lab 1/src/link.js
@@ -1,4 +1,5 @@
 while (true) {
+    console.log(chocolates);
     console.log("CHOCOLATE DISPENSER");
     console.log(" 1. Add");
     console.log(" 2. Remove");
@@ -10,29 +11,51 @@ while (true) {
     console.log(" 8. Change color (all)");
     console.log(" 9. Remove (color)");
     console.log("10. Dispense (rainbow)");
-    //let choice = prompt("Enter choice");
-    console.log(choice);
-    switch (choice) {
+    var choice = prompt("Enter choice");
+    switch (Number(choice)) {
         case 1:
-            console.log("Hey!");
+            var color = prompt("Enter color");
+            var count = prompt("Enter count");
+            addChocolates(chocolates, color, count);
             break;
         case 2:
+            var number = prompt("Enter number");
+            console.log("Chocolates removed: " + removeChocolates(chocolates, number));
             break;
         case 3:
+            var number = prompt("Enter number");
+            console.log("Chocolates dispensed: " + dispenseChocolates(chocolates, number));
             break;
         case 4:
+            var color = prompt("Enter color");
+            var number = prompt("Enter count");
+            console.log("Chocolates dispensed: " + dispenseChocolatesOfColor(chocolates, number, color));
             break;
         case 5:
+            var count = noOfChocolates(chocolates);
+            console.log("Green: " + count[0] + " Red: " + count[1] + " Purple: " + count[2] + " Blue: " + count[3] + " Crimson: " + count[4] + " Silver: " + count[5] + " Pink: " + count[6]);
             break;
         case 6:
+            sortChocolateBasedOnCount(chocolates);
             break;
         case 7:
+            var count = prompt("Number of chocolates to change");
+            var currentColor = prompt("Current color");
+            var finalColor = prompt("Desired color");
+            changeChocolateColor(chocolates, count, currentColor, finalColor);
             break;
         case 8:
+            var currentColor = prompt("Current color");
+            var finalColor = prompt("Desired color");
+            changeChocolateColorAllOfxCount(chocolates, currentColor, finalColor);
             break;
         case 9:
+            var color = prompt("Color to be removed");
+            console.log("Chocolate removed: " + removeChocolateOfColor(chocolates, color));
             break;
         case 10:
+            var count = prompt("Chocolates to dispense");
+            console.log("Rainbow chocolates dispensed: " + count);
             break;
     }
 }
